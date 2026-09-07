7f67618b1dc4be0bcbd482c8539715cf40232d42
nsufiyan <shaiknizamuddinsufiyan@gmail.com>
2026-09-07 18:36:51 +0530

    palindrom check

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,14 @@
-s = "malayalam" 
+def isPalindrome(str1):
+    i=0
+    j=len(str1)-1
 
-if s == s[::-1]:
-    print("Yes")
-else:
-    print("No")
+    while i<=j:
+        if str1[i]==str1[j]:
+           i=i+1
+           j=j-1
+        else:
+           return False
+    return True
+
+str1=input("enter the string:")
+print(isPalindrome(str1))
